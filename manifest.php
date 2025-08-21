c2848459f058eacf85be8e66ce0877e1890cdb70
jonmlas <jonmlas@gmail.com>
2025-08-21 18:58:23 +0800

    Manifest updated

--- manifest.php
+++ manifest.php
@@ -5,18 +5,34 @@
 $manifest = array();
 
 $manifest['name']        = __( 'Mega Menu', 'fw' );
-$manifest['description'] = __( 'The Mega Menu extension adds a user-friendly drop down menu that will let you easily create highly customized menu configurations.', 'fw' );
-$manifest['version'] = '1.1.3';
-$manifest['display'] = true;
-$manifest['github_repo'] = 'https://github.com/ThemeFuse/Unyson-MegaMenu-Extension';
-$manifest['uri'] = 'http://manual.unyson.io/en/latest/extension/megamenu/index.html#content';
-$manifest['author'] = 'ThemeFuse';
-$manifest['author_uri'] = 'http://themefuse.com/';
-$manifest['standalone'] = true;
+$manifest['slug']        = 'unysonplus-megamenu';
+$manifest['description'] = __( 
+	'The Mega Menu extension adds a user-friendly drop down menu that will let you easily create highly customized menu configurations.', 
+	'fw' 
+);
+
+$manifest['version']     = '1.1.3';
+$manifest['display']     = true;
+$manifest['standalone']  = true;
+
+// Repository Info
+$manifest['github_update'] = 'UnysonPlus/UnysonPlus-MegaMenu-Extension';
+$manifest['github_repo']   = 'https://github.com/UnysonPlus/UnysonPlus-MegaMenu-Extension';
+$manifest['github_branch'] = 'master';
+
+// Author Info
+$manifest['author']     = 'UnysonPlus';
+$manifest['author_uri'] = 'https://www.lastimosa.com.ph/unysonplus';
+
+// Requirements
 $manifest['requirements'] = array(
 	'framework' => array(
 		'min_version' => '2.5.9', // class FW_Db_Options_Model
 	),
 );
 
-$manifest['github_update'] = 'ThemeFuse/Unyson-MegaMenu-Extension';
+// Meta
+$manifest['license']      = 'GPL-2.0-or-later';
+$manifest['text_domain']  = 'fw';
+$manifest['requires_php'] = '7.4';
+$manifest['requires_wp']  = '5.8';
